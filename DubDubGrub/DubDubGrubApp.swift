f1f607ce9b22408e166431828cc8cb3062be5c1c
Abdulaziz Alobaili <dr.alobaili@gmail.com>
2023-03-20 23:03:08 +0300

    Initial Commit

---
+++ DubDubGrub/DubDubGrubApp.swift
@@ -0,0 +1,17 @@
+//
+//  DubDubGrubApp.swift
+//  DubDubGrub
+//
+//  Created by Abdulaziz Alobaili on 20/03/2023.
+//
+
+import SwiftUI
+
+@main
+struct DubDubGrubApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
