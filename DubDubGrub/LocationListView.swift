e1a801c35f927dd485e32ea902fc7d90c8d08794
Abdulaziz Alobaili <dr.alobaili@gmail.com>
2023-03-20 23:23:09 +0300

    Add initial tab view

---
+++ DubDubGrub/LocationListView.swift
@@ -0,0 +1,20 @@
+//
+//  LocationListView.swift
+//  DubDubGrub
+//
+//  Created by Abdulaziz Alobaili on 20/03/2023.
+//
+
+import SwiftUI
+
+struct LocationListView: View {
+    var body: some View {
+        Text("List View")
+    }
+}
+
+struct LocationListView_Previews: PreviewProvider {
+    static var previews: some View {
+        LocationListView()
+    }
+}
---
+++ DubDubGrub/ProfileView.swift
@@ -0,0 +1,20 @@
+//
+//  ProfileView.swift
+//  DubDubGrub
+//
+//  Created by Abdulaziz Alobaili on 20/03/2023.
+//
+
+import SwiftUI
+
+struct ProfileView: View {
+    var body: some View {
+        Text("Profile")
+    }
+}
+
+struct ProfileView_Previews: PreviewProvider {
+    static var previews: some View {
+        ProfileView()
+    }
+}
