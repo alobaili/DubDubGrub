8a9c9faf9efd94e0635a3a466fa8f9dfcda26d83
Abdulaziz Alobaili <dr.alobaili@gmail.com>
2023-03-20 23:16:04 +0300

    Add brandPrimary color asset

---
+++ DubDubGrub/Color+.swift
@@ -0,0 +1,12 @@
+//
+//  Color+.swift
+//  DubDubGrub
+//
+//  Created by Abdulaziz Alobaili on 20/03/2023.
+//
+
+import SwiftUI
+
+extension Color {
+    static let brandPrimary = Color("brandPrimary")
+}
