25b70910ca370283fcd675160e1f9d25371d79c4
Abdulaziz Alobaili <dr.alobaili@gmail.com>
2023-03-21 17:16:41 +0300

    Make the faces grid scrollable

--- DubDubGrub/LocationDetailView.swift
+++ DubDubGrub/LocationDetailView.swift
@@ -11,63 +11,64 @@ struct LocationDetailView: View {
     let columns = [GridItem(.flexible()), GridItem(.flexible()), GridItem(.flexible())]
 
     var body: some View {
-        NavigationView {
-            VStack(spacing: 16) {
-                Image("default-banner-asset")
-                    .resizable()
-                    .scaledToFill()
-                    .frame(height: 120)
+        VStack(spacing: 16) {
+            Image("default-banner-asset")
+                .resizable()
+                .scaledToFill()
+                .frame(height: 120)
 
-                HStack {
-                    Label("123 Main Street", systemImage: "mappin.and.ellipse")
-                        .font(.caption)
-                        .foregroundColor(.secondary)
+            HStack {
+                Label("123 Main Street", systemImage: "mappin.and.ellipse")
+                    .font(.caption)
+                    .foregroundColor(.secondary)
 
-                    Spacer()
-                }
+                Spacer()
+            }
+            .padding(.horizontal)
+
+            Text("This is a test description. This is a test description. This is a test description. This is a test description. This is a test description.")
+                .lineLimit(3)
+                .minimumScaleFactor(0.75)
+                .frame(height: 70)
                 .padding(.horizontal)
 
-                Text("This is a test description. This is a test description. This is a test description. This is a test description. This is a test description.")
-                    .lineLimit(3)
-                    .minimumScaleFactor(0.75)
-                    .padding(.horizontal)
+            ZStack {
+                Capsule()
+                    .frame(height: 80)
+                    .foregroundColor(Color(uiColor: .secondarySystemBackground))
 
-                ZStack {
-                    Capsule()
-                        .frame(height: 80)
-                        .foregroundColor(Color(uiColor: .secondarySystemBackground))
+                HStack(spacing: 20) {
+                    Button {
 
-                    HStack(spacing: 20) {
-                        Button {
+                    } label: {
+                        LocationActionButton(color: .brandPrimary, imageName: "location.fill")
+                    }
 
-                        } label: {
-                            LocationActionButton(color: .brandPrimary, imageName: "location.fill")
-                        }
+                    Link(destination: URL(string: "https://www.apple.com")!) {
+                        LocationActionButton(color: .brandPrimary, imageName: "network")
 
-                        Link(destination: URL(string: "https://www.apple.com")!) {
-                            LocationActionButton(color: .brandPrimary, imageName: "network")
+                    }
 
-                        }
+                    Button {
 
-                        Button {
+                    } label: {
+                        LocationActionButton(color: .brandPrimary, imageName: "phone.fill")
+                    }
 
-                        } label: {
-                            LocationActionButton(color: .brandPrimary, imageName: "phone.fill")
-                        }
+                    Button {
 
-                        Button {
-
-                        } label: {
-                            LocationActionButton(color: .brandPrimary, imageName: "person.fill.checkmark")
-                        }
+                    } label: {
+                        LocationActionButton(color: .brandPrimary, imageName: "person.fill.checkmark")
                     }
                 }
-                .padding(.horizontal)
+            }
+            .padding(.horizontal)
 
-                Text("Who's Here")
-                    .bold()
-                    .font(.title2)
+            Text("Who's Here")
+                .bold()
+                .font(.title2)
 
+            ScrollView {
                 LazyVGrid(columns: columns) {
                     FirstNameAvatarView(firstName: "Abdulaziz")
                     FirstNameAvatarView(firstName: "Abdulaziz")
@@ -77,18 +78,20 @@ struct LocationDetailView: View {
                     FirstNameAvatarView(firstName: "Abdulaziz")
                     FirstNameAvatarView(firstName: "Abdulaziz")
                 }
-
-                Spacer()
             }
-            .navigationTitle("Location Name")
-            .navigationBarTitleDisplayMode(.inline)
+
+            Spacer()
         }
+        .navigationTitle("Location Name")
+        .navigationBarTitleDisplayMode(.inline)
     }
 }
 
 struct LocationDetailView_Previews: PreviewProvider {
     static var previews: some View {
-        LocationDetailView()
+        NavigationView {
+            LocationDetailView()
+        }
     }
 }
 
